13dc1692d6db061dad0113b454d0ba862c310f17
Аудитория 413 <u413@stkt.spbsut.ru>
2021-10-26 11:49:54 +0300

    Добавьте файлы проекта.

---
+++ BlackBoxTesting/ExpressionCalculator.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BlackBoxTesting
+{
+    public static class ExpressionCalculator
+    {
+        public static void Calculate(double a, double b, double c, out double x1, out double x2)
+        {
+            x1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+            x2 = (-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+        }
+        /// <summary>
+        /// Скалярное произведение вектора
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns> 
+        public static double GetSkalyar(Point v1, Point v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y;
+        }
+        /// <summary>
+        /// Расстояние между точками
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double GetLenght(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+        }
+        /// <summary>
+        /// Модуль вектора
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static double GetModule(Point v)
+        {
+            return Math.Sqrt(v.X * v.X + v.Y * v.Y);
+        }
+        /// <summary>
+        /// Угол между векторами
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static double GetAngle(Point v1, Point v2)
+        {
+            return Math.Acos(GetSkalyar(v1, v2) / (GetModule(v1) * GetModule(v2)));
+        }
+        /// <summary>
+        /// Находит вектор прямой заданной двумя точками
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Point GetVector(Point a, Point b)
+        {
+            return new Point(a.X - b.X, a.Y - b.Y);
+        }
+        /// <summary>
+        /// Проверка на ромб
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static bool IsRomb(Point a, Point b, Point c, Point d)
+        {
+            double ab = GetLenght(a,b), bc = GetLenght(b, c), cd = GetLenght(c, d), da = GetLenght(d, a);
+            return ab == bc && bc == cd && cd == da;
+        }
+        /// <summary>
+        /// Проверка на квадрат
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static bool IsSquare(Point a, Point b, Point c, Point d)
+        {
+            Point v1 = GetVector(b, a);
+            Point v2 = GetVector(b, c);
+            Point v3 = GetVector(c, b);
+            Point v4 = GetVector(c, d);
+            return GetSkalyar(v1, v2) == 0 && GetSkalyar(v3,v4) == 0 && IsRomb(a, b, c, d);
+        }
+        /// <summary>
+        /// Проверка на коллинеарность векторов
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool IsColleniar(Point v1, Point v2)
+        {
+            if(v1.Y == 0 || v2.Y == 0)
+                return v1.Y / v1.X == v2.Y / v2.X;
+            else
+                return v1.X / v1.Y == v2.X / v2.Y;
+        }
+        /// <summary>
+        /// Проверка на парралелограм
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static bool IsParallelogram(Point a, Point b, Point c, Point d)
+        {
+            Point v1 = GetVector(a, b);
+            Point v2 = GetVector(c, d);
+            Point v3 = GetVector(b, c);
+            Point v4 = GetVector(d, a);
+            return IsColleniar(v1, v2) && IsColleniar(v3, v4);
+        }
+    }
+}
---
+++ BlackBoxTesting/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlackBoxTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRes2_Click(object sender, RoutedEventArgs e)
+        {
+            double x1, x2, x3, x4, y1, y2, y3, y4;
+            x1 = double.Parse(TxbP1x.Text);
+            x2 = double.Parse(TxbP2x.Text);
+            x3 = double.Parse(TxbP3x.Text);
+            x4 = double.Parse(TxbP4x.Text);
+            y1 = double.Parse(TxbP1y.Text);
+            y2 = double.Parse(TxbP2y.Text);
+            y3 = double.Parse(TxbP3y.Text);
+            y4 = double.Parse(TxbP4y.Text);
+            Point p1, p2, p3, p4;
+            p1 = new Point(x1, y1);
+            p2 = new Point(x2, y2);
+            p3 = new Point(x3, y3);
+            p4 = new Point(x4, y4);
+            if (ExpressionCalculator.IsSquare(p1, p2, p3, p4))
+            {
+                TxbResult2.Text = "Square";
+            }
+            else if (ExpressionCalculator.IsRomb(p1, p2, p3, p4))
+            {
+                TxbResult2.Text = "Romb";
+            }
+            else if (ExpressionCalculator.IsParallelogram(p1, p2, p3, p4))
+            {
+                TxbResult2.Text = "Parallelogram";
+            }
+        }
+
+        private void BtnRes1_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TxbA.Text, out double a) && double.TryParse(TxbB.Text, out double b) && double.TryParse(TxbC.Text, out double c))
+            {
+                ExpressionCalculator.Calculate(a, b, c, out double x1, out double x2);
+                TxbResult1.Text = $"x1 = {x1:N2}\n" + $"x2 = {x2:N2}";
+            }
+        }
+    }
+}
