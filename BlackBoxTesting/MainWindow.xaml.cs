2e08affa92a258c461102334e8c95ed46d4ce87a
Тимур Гиреев <gireev2003@mail.ru>
2021-10-28 15:30:21 +0300

    Создание ошибок

--- BlackBoxTesting/MainWindow.xaml.cs
+++ BlackBoxTesting/MainWindow.xaml.cs
@@ -27,10 +27,22 @@ namespace BlackBoxTesting
 
         private void BtnRes2_Click(object sender, RoutedEventArgs e)
         {
-            Point p1 = new Point(double.Parse(TxbP1x.Text), double.Parse(TxbP1y.Text));
-            Point p2 = new Point(double.Parse(TxbP2x.Text), double.Parse(TxbP2y.Text));
-            Point p3 = new Point(double.Parse(TxbP3x.Text), double.Parse(TxbP3y.Text));
-            Point p4 = new Point(double.Parse(TxbP4x.Text), double.Parse(TxbP4y.Text));
+            Point p2;
+            Point p1;
+            Point p3;
+            Point p4;
+            try
+            {
+                p2 = new Point(double.Parse(TxbP1x.Text), double.Parse(TxbP1y.Text));
+                p1 = new Point(double.Parse(TxbP2x.Text), double.Parse(TxbP2y.Text));
+                p3 = new Point(double.Parse(TxbP3x.Text), double.Parse(TxbP3y.Text));
+                p4 = new Point(double.Parse(TxbP4x.Text), double.Parse(TxbP4y.Text));
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            
             if (ExpressionCalculator.IsSquare(p1, p2, p3, p4))
             {
                 TxbResult2.Text = "Square";
@@ -67,11 +79,50 @@ namespace BlackBoxTesting
 
         private void BtnRes1_Click(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(TxbA.Text, out double a) && double.TryParse(TxbB.Text, out double b) && double.TryParse(TxbC.Text, out double c))
+            double c = 0;
+            if ( double.TryParse(TxbB.Text, out double b))
             {
+                string txbAtext = ""; 
+                if (!double.TryParse(TxbA.Text, out double a))
+                {
+                    for (int i = 0; i < TxbA.Text.Length; i++)
+                    {
+                        if (char.IsDigit(TxbA.Text[i]))
+                        {
+                            txbAtext += TxbA.Text[i];
+                        }
+                    }
+                    double.TryParse( txbAtext,out a);
+                }
+                if (TxbC.Text.Length == 0)
+                {
+                    
+                }
+                else if(!double.TryParse(TxbC.Text, out c))
+                {
+                    return;
+                }
                 ExpressionCalculator.Calculate(a, b, c, out double x1, out double x2);
                 TxbResult1.Text = $"x1 = {x1:N2}\n" + $"x2 = {x2:N2}";
             }
+        }
+
+        private void Txb_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key <= Key.D0 || e.Key >= Key.D9) && e.Key != Key.OemMinus)
+            { 
+                e.Handled = true;
+            }
+        }
+
+        private void TxbA_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key <= Key.D0 || e.Key >= Key.D9)&& e.Key != Key.OemMinus)
+            {
+                e.Handled = true;
+            }
+            if (e.Key >= Key.A && e.Key <= Key.Z)
+                e.Handled = false;
         }
     }
 }
